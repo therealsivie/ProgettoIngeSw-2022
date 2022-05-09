b4a2dfc55bc0bfe2fb3d474ac459eb91ef367409
miche-linux <m.ce@studenti.unibs.it>
2022-05-09 22:03:26 +0200

    Correzione per poter iniziare i test di v1

--- src/data/DbConnect.java
+++ src/data/DbConnect.java
@@ -4,7 +4,7 @@ import structure.*;
 import java.sql.*;
 
 public class DbConnect {
-    String url = "jdbc:sqlite:/home/alessandro/JavaProjects/ProgettoIngeSw-2022/Data.db";
+    String url = "jdbc:sqlite:./Data.db";
     public Connection connect() {
         Connection conn = null;
         try {
